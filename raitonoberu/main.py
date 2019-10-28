4fcac1f45d7cae69fa657a02defb88b8388ec466
arasHi87 <arasi27676271@gmail.com>
2019-10-29 01:47:55 +0800

    add wenku detail function

--- raitonoberu/main.py
+++ raitonoberu/main.py
@@ -9,6 +9,12 @@ def main():
     opt = arg_parser.parser()
     if opt.search_key is not None:
         wenku_parser.searcher(opt.search_key)
+    elif opt.search_detail is not None:
+        search_detail = opt.search_detail.split(':')
+        source = search_detail[0]
+        _id = search_detail[1]
+        if source == 'wenku':
+            wenku_parser.detail(_id)
 
 
 if __name__ == '__main__':
