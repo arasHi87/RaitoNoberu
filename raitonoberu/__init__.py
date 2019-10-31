6d05b5a66941f98b2c5a416fbe44865a4f78122a
arasHi87 <arasi27676271@gmail.com>
2019-10-31 11:09:03 +0800

    remove raitonoberu

--- raitonoberu/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'arasHi_'
-__email__ = 'arasi27676271@gmail.com'
-__version__ = '0.0.1'
--- raitonoberu/mega/test.py
+++
@@ -1,6 +0,0 @@
-from mega import Mega
-mega = Mega()
-m = mega.login()
-m.download_url(
-    'https://mega.co.nz/#!4YBVHKyb!lBI7GbEYs40I12Jczy3dp_NIEDg9kYuolmT5nzoWwOQ'
-)
