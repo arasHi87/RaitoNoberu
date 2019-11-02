dea8f5934a3523741448e52a1e3ca719be339599
arasHi87 <arasi27676271@gmail.com>
2019-11-02 09:29:04 +0800

    code style format

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import setup, find_packages, Extension
 from lightdo import __version__, __author__, __email__
 
-
 with open('requirements.txt') as f:
     requirements = [l for l in f.read().splitlines() if l]
 
 with open("README.md", encoding='utf-8') as file:
     long_description = file.read()
 
-setup (
-    name = 'lightdo',
-    version = __version__,
-    keywords = ['lightdo', 'raito', 'raitonoberu', 'light novel downloader', 'novel'],
-    description = 'light novel downloader',
-    author = __author__,
-    author_email = __email__,
-    license = 'MIT',
-    url = 'https://github.com/arasHi87/RaitoNoberu',
-    long_description = long_description,
-    long_description_content_type='text/markdown',
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires = requirements,
-    entry_points={
-        'console_scripts': [
-            'lightdo = lightdo.main:main',
-        ]
-    }
-)
+setup(name='lightdo',
+      version=__version__,
+      keywords=[
+          'lightdo', 'raito', 'raitonoberu', 'light novel downloader', 'novel'
+      ],
+      description='light novel downloader',
+      author=__author__,
+      author_email=__email__,
+      license='MIT',
+      url='https://github.com/arasHi87/RaitoNoberu',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      entry_points={'console_scripts': [
+          'lightdo = lightdo.main:main',
+      ]})
